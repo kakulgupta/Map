73f43d51b932970764ae094836a909c8b84b4f21
KAKUL GUPTA <kakul.gupta009@gmail.com>
2015-02-05 10:50:00 +0530

    added orientation for robot

--- lib/Survey/robot.rb
+++ lib/Survey/robot.rb
@@ -1,10 +1,11 @@
 class Robot
-  def initialize(xinit, yinit)
+  def initialize(xinit, yinit, orientation)
     @xinit = xinit
     @yinit = yinit
+    @orientation = orientation
   end
 
   def initial_pos
-    return [@xinit, @yinit]
+    return [@xinit, @yinit, @orientation]
   end
 end
--- spec/Survey/robot_spec.rb
+++ spec/Survey/robot_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe "Robot" do
-  let(:robot1){Robot.new(2, 3)}
+  let(:robot1){Robot.new(2, 3, "east")}
   it "checks initial position" do
-    expect(robot1.initial_pos).to eq([2 ,3])
+    expect(robot1.initial_pos).to eq([2, 3, "east"])
   end
-  
+
 end
