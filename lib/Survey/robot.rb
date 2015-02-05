9e344ed5ab6574fbd856746c57116ee1f2f40154
KAKUL GUPTA <kakul.gupta009@gmail.com>
2015-02-05 11:01:38 +0530

    overloaded == and eql?

--- lib/Survey/robot.rb
+++ lib/Survey/robot.rb
@@ -1,11 +1,24 @@
 class Robot
-  def initialize(xinit, yinit, orientation)
-    @xinit = xinit
-    @yinit = yinit
+  attr_reader :x, :y, :orientation
+  def initialize(x, y, orientation)
+    @x = x
+    @y = y
     @orientation = orientation
   end
 
   def initial_pos
-    return [@xinit, @yinit, @orientation]
+    return self  
+  end
+
+  def ==(other)
+    self.x == other.x
+    self.y == other.y
+    self.orientation == other.orientation
+  end
+
+  def eql?(other)
+    self.x == other.x
+    self.y == other.y
+    self.orientation == other.orientation
   end
 end
--- spec/Survey/robot_spec.rb
+++ spec/Survey/robot_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe "Robot" do
   let(:robot1){Robot.new(2, 3, "east")}
   it "checks initial position" do
-    expect(robot1.initial_pos).to eq([2, 3, "east"])
+    expect(robot1.initial_pos).to eq(Robot.new(2, 3, "east"))
   end
 
+  #it "checks move left" do
+    #expect(robot1.move_left).to eq(Robot.new(2, 4, "north"))
+  #end
 end
